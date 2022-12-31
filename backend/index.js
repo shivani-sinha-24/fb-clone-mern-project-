cc6c9c7019a2f2d9aee61e4c9cf8905c6e02ab0b
ShivaniSinha <shivanisinha24dec@gmail.com>
2023-01-01 00:52:08 +0530

    specified the location of uploads directory in the backend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/uploads", express.static("./uploads"));
+app.use("/uploads", express.static("./uploads"));  // upload folder should be in is in root directory of backed folder
 
 const saltRounds = 10;
 
